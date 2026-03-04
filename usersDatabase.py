61076353b0de793ed736f6eb5add94c80be0cb96
vidmahi <87253286+vidmahi@users.noreply.github.com>
2026-03-04 09:23:27 -0600

    All functions combined

--- usersDatabase.py
+++ usersDatabase.py
@@ -16,12 +16,42 @@ User = {
 # Function to add a new user
 def addUser(client, username, userId, password):
     # Add a new user to the database
-    pass
+    db = client["HaaS_DB"]
+    users = db["users"]
+
+    #Check if user already exists
+    exist_user = __queryUser(client, username, userId)
+    if exist_user is not None:
+        return False #The user already exists
+    
+    #Otherwise, Hash password like def login
+    salt = hashlib.sha256(username.encode()).digest()
+    password_hash = hashlib.pbkdf2_hmac(
+        "sha256",
+        password.encode(),
+        salt,
+        100_000
+    )
+
+    #Create user document
+    user = {
+        "username": username, 
+        "userId": userId, 
+        "password_hash": password_hash, 
+        "salt": salt, 
+        "projects": []
+    }
+
+    users.insert_one(user)
+    return True #User has been added!
 
 # Helper function to query a user by username and userId
 def __queryUser(client, username, userId):
     # Query and return a user from the database
-    pass
+     db = client["HaaS_DB"]
+     users = db["users"]
+     return users.find_one({"username": username, "userId": userId}) 
+
 
 # Function to log in a user
 def login(client, username, userId, password):
