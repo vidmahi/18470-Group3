2071bba5f521697a7ee0a9f213fcd6865de555f4
vidmahi <vidmahis@gmail.com>
2026-04-06 14:20:25 -0500

    Add production API URL

--- backend/app.py
+++ backend/app.py
@@ -1035,6 +1035,9 @@ def inventory():
     return jsonify({"success": True, "inventory": inventory_data})
 
 
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=True)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
